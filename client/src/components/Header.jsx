e5e9f22c62d52a09069570876a6f7750d14c9dac
Adarsh nm <adarshnm02@gmail.com>
2024-08-05 15:31:47 +0530

    update header components and make profile privet

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector(state => state.user)
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,9 +15,14 @@ const Header = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
+            <Link to="/profile">
+            {currentUser? (
+              <img src={currentUser.profilePicture} alt="Profile" className="h-7 w-7 rounded-full object-cover" />
+            ):(
+              <li>Sign In</li>
+
+            )}
+            </Link>
         </ul>
       </div>
     </div>
